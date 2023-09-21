blurayne/ghostty

commit fbe030d85a80afa7818884f942d124d856224013

terminal: respond to XTVERSION query
XTVERSION (CSI > 0 q) is used by some libraries to identify the terminal
+ version. Respond to this query with `ghostty {version_string}`. There
is no formal format for this response. A roundup of a few tested
terminals show two primary formats. This patch opts to save one byte and
use the `name SP version` semantics.

foot: foot(version)
xterm: XTerm(version)
contour: contour version
wezterm: wezterm version

Reference: https://github.com/dankamongmen/notcurses/blob/master/TERMINALS.md#notes-for-terminal-authors
Signed-off-by: Tim Culverhouse <tim@timculverhouse.com>

src/termio/Exec.zig
@@ -1688,6 +1688,23 @@ const StreamHandler = struct {
         self.terminal.screen.kitty_keyboard.set(mode, flags);
     }
 
+    pub fn reportXtversion(
+        self: *StreamHandler,
+    ) !void {
+        log.debug("reporting XTVERSION: ghostty {s}", .{build_config.version_string});
+        var msg: termio.Message = .{ .write_small = .{} };
+        const resp = try std.fmt.bufPrint(
+            &msg.write_small.data,
+            "\x1BP>|{s} {s}\x07",
+            .{
+                "ghostty",
+                build_config.version_string,
+            },
+        );
+        msg.write_small.len = @intCast(resp.len);
+        self.messageWriter(msg);
+    }
+
     //-------------------------------------------------------------------------
     // OSC
 

src/terminal/stream.zig
@@ -607,28 +607,32 @@ pub fn Stream(comptime Handler: type) type {
                     ),
                 },
 
-                // DECSCUSR - Select Cursor Style
-                // TODO: test
                 'q' => switch (action.intermediates.len) {
-                    1 => cursor: {
-                        if (action.intermediates[0] != ' ') {
+                    1 => switch (action.intermediates[0]) {
+                        // DECSCUSR - Select Cursor Style
+                        // TODO: test
+                        ' ' => {
+                            if (@hasDecl(T, "setCursorStyle")) try self.handler.setCursorStyle(
+                                switch (action.params.len) {
+                                    0 => ansi.CursorStyle.default,
+                                    1 => @enumFromInt(action.params[0]),
+                                    else => {
+                                        log.warn("invalid set curor style command: {}", .{action});
+                                        return;
+                                    },
+                                },
+                            ) else log.warn("unimplemented CSI callback: {}", .{action});
+                        },
+                        // XTVERSION
+                        '>' => {
+                            if (@hasDecl(T, "reportXtversion")) try self.handler.reportXtversion();
+                        },
+                        else => {
                             log.warn(
                                 "ignoring unimplemented CSI q with intermediates: {s}",
                                 .{action.intermediates},
                             );
-                            break :cursor;
-                        }
-
-                        if (@hasDecl(T, "setCursorStyle")) try self.handler.setCursorStyle(
-                            switch (action.params.len) {
-                                0 => ansi.CursorStyle.default,
-                                1 => @enumFromInt(action.params[0]),
-                                else => {
-                                    log.warn("invalid set curor style command: {}", .{action});
-                                    return;
-                                },
-                            },
-                        ) else log.warn("unimplemented CSI callback: {}", .{action});
+                        },
                     },
 
                     else => log.warn(

commit 29e778db04049874c2dc2b080ff5803b65aa971f

terminal: use larger buffer for xtversion response
Commit fbe030d85a80 ("terminal: respond to XTVERSION query") introduced
responding to XTVERSION queries. The implementation uses the
.write_small method, which has a limit of 38 bytes. This works well if
your branch is named "main", since the branch is part of the
version_string variable. If you start using longer branch names, you can
quickly run into the limit.

The XTVERSION response is:

  "\x1bP>|ghostty d.d.d-<branch>+<12-digit-hash>\x07"

Which has an overhead of 32 bytes, meaning the natural branch limit is 6
bytes (6 characters, assuming you use ASCII branch names). Github has a
limit of 256 chars, so let's set a max XTVERSION buffer of 256+32 = 288

Fixes: fbe030d85a80 ("terminal: respond to XTVERSION query")

src/termio/Exec.zig
@@ -1692,16 +1692,16 @@ const StreamHandler = struct {
         self: *StreamHandler,
     ) !void {
         log.debug("reporting XTVERSION: ghostty {s}", .{build_config.version_string});
-        var msg: termio.Message = .{ .write_small = .{} };
+        var buf: [288]u8 = undefined;
         const resp = try std.fmt.bufPrint(
-            &msg.write_small.data,
+            &buf,
             "\x1BP>|{s} {s}\x07",
             .{
                 "ghostty",
                 build_config.version_string,
             },
         );
-        msg.write_small.len = @intCast(resp.len);
+        var msg = try termio.Message.writeReq(self.alloc, resp);
         self.messageWriter(msg);
     }
 